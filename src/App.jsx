434f1c214e9f4844e643528e5fda4a701fa1d243
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-03-01 16:24:43 +0000

    add: Bootstrap navbar added to project

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,49 @@ import {Home} from "/reactportfolio/src/Pages/Home";
 import {Courses} from "/reactportfolio/src/Pages/Courses";
 import {Projects} from "/reactportfolio/src/Pages/Projects";
 import {Contact} from "/reactportfolio/src/Pages/Contact";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as bootstrap from 'bootstrap'
+
+
 
 function App() {
 
-
+ 
   return (
     <>
-    <div className="container"></div>
-      <nav>
-        <ul>
-            <li> <Link to="/reactportfolio">Home</Link></li>
-            <li> <Link to="/reactportfolio/courses">Courses</Link></li>
-            <li> <Link to="/reactportfolio/projects">Projects</Link></li>
-            <li> <Link to="/reactportfolio/contact">Contact</Link></li>
-
-        </ul>
-
-      </nav>
+  
+      <nav className ="navbar fixed-top navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">React Portfolio</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</nav>
 
 <Routes>
         <Route path="/reactportfolio" element={<Home />} />
