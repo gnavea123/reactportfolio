6b6910cd998c6bd7688894596c6a72152c8c0dad
Gaston Navea <gaston_navea@yahoo.co.uk>
2024-03-01 17:03:01 +0000

    modify: changed node v2 to nodev4 so WFL uses Node:v20

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 import React from "react";
 import {Link} from "react-router-dom";
 import {Route, Routes} from "react-router-dom"
-import {Home} from "/reactportfolio/src/Pages/Home";
+import {Home} from "./Pages/Home";
 import {Courses} from "/reactportfolio/src/Pages/Courses";
 import {Projects} from "/reactportfolio/src/Pages/Projects";
 import {Contact} from "/reactportfolio/src/Pages/Contact";
